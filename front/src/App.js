5c8aebcf7461f912f68438a9774696861e1bc3fe
cadsduarte <cadsduarte@gmail.com>
2024-04-06 12:09:42 -0300

    projeto-front-react

---
+++ front/src/App.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
+
+function App() {
+
+  // objeto produto
+  const produto = {
+    codigo : 0,
+    nome : '',
+    marca : ''
+  }
+
+
+  //  useState
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjProduto] = useState(produto);
+
+  //  useEffect
+  useEffect(()=>{
+    fetch("http://localhost:8080/listar")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
+
+  // Obtendo os dados do formulário
+  const aoDigitar = (e) => {
+    
+    setObjProduto({...objProduto, [e.target.name] : e.target.value});
+  }
+
+  // Cadastrar produtos
+  const cadastrar = () => {
+    fetch('http://localhost:8080/cadastrar',{
+      method:'post',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      
+      if(retorno_convertido.mensagem !== undefined){
+        alert(retorno_convertido.mensagem);
+      }else{
+        setProdutos([...produtos, retorno_convertido]);
+        alert('Produto cadastrado com sucesso !')
+        limparFormulario();
+      }
+    })
+  }
+
+  // limpar formulário
+  const limparFormulario = () => {
+    setObjProduto(produto);
+  }
+
+  // Selecionar formulário
+  const selecionarProduto = (indice) => {
+    setObjProduto(produtos[indice]);
+    setBtnCadastrar(false);
+  }
+
+  // Retorno
+  return (
+    <div>
+      <Formulario botao = {btnCadastrar} eventoTeclado = {aoDigitar} cadastrar = {cadastrar} obj={objProduto}/>
+      <Tabela vetor = {produtos} selecionar = {selecionarProduto}/>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ front/src/Formulario.js
@@ -0,0 +1,25 @@
+function Formulario({botao, eventoTeclado, cadastrar, obj}){
+    return(
+        <form>
+            <input type='text' value={obj.nome} onChange={eventoTeclado} name = 'nome' placeholder='Nome' className = 'form-control'/>
+            <input type='text' value={obj.marca} onChange={eventoTeclado} name = 'marca' placeholder='Marca'className = 'form-control'/>
+     
+             {
+
+                botao
+                ?
+                  <input type='button' value = 'Cadastrar' onClick={cadastrar} className='btn btn-primary'/>
+                :
+                <div>
+                  <input type='button' value = 'Alterar'   className='btn btn-warning'/>
+                  <input type='button' value = 'Remover'   className='btn btn-danger'/>
+                  <input type='button' value = 'Cancelar'  className='btn btn-secondary'/>
+                </div>
+             }
+
+            
+        </form>
+    )
+}
+
+export default Formulario;
