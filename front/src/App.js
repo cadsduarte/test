7e88f9332875094dc2b4611a79aee41af55d2615
cadsduarte <cadsduarte@gmail.com>
2024-04-20 18:23:22 -0300

    Projeto front-react concluido

--- front/src/App.js
+++ front/src/App.js
@@ -55,9 +55,85 @@ function App() {
     })
   }
 
+  // Alterar produtos
+  const alterar = () => {
+    fetch('http://localhost:8080/alterar',{
+      method:'put',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      
+      if(retorno_convertido.mensagem !== undefined){
+        alert(retorno_convertido.mensagem);
+      }else{
+        
+        // mensagem
+        alert('Produto alterado com sucesso !');
+
+        // cópia do vetor de produtos
+      let vetorTemp = [...produtos];
+
+      // Indice
+      let indice = vetorTemp.findIndex((p)=>{
+        return p.codigo === objProduto.codigo;
+      });
+
+      //  alterar produto do vetorTemp
+      vetorTemp[indice] = objProduto;
+
+      // Atualizar o vetor de produtos
+      setProdutos(vetorTemp);
+
+        // limpar o formulário
+        limparFormulario();
+      }
+    })
+  }
+
+  // Remover produtos
+  const remover = () => {
+    fetch('http://localhost:8080/remover/'+objProduto.codigo,{
+      method:'delete',
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      
+      // Mensagem
+      alert(retorno_convertido.mensagem);
+
+      // cópia do vetor de produtos
+      let vetorTemp = [...produtos];
+
+      // Indice
+      let indice = vetorTemp.findIndex((p)=>{
+        return p.codigo === objProduto.codigo;
+      });
+
+      //  Remover produto do vetorTemp
+      vetorTemp.splice(indice, 1);
+
+      // Atualizar o vetor de produtos
+      setProdutos(vetorTemp);
+
+      // Limpar formulário
+      limparFormulario();
+
+    })
+  }
+
   // limpar formulário
   const limparFormulario = () => {
     setObjProduto(produto);
+    setBtnCadastrar(true);
   }
 
   // Selecionar formulário
@@ -69,7 +145,7 @@ function App() {
   // Retorno
   return (
     <div>
-      <Formulario botao = {btnCadastrar} eventoTeclado = {aoDigitar} cadastrar = {cadastrar} obj={objProduto}/>
+      <Formulario botao = {btnCadastrar} eventoTeclado = {aoDigitar} cadastrar = {cadastrar} obj={objProduto} cancelar={limparFormulario} remover={remover} alterar={alterar}  />
       <Tabela vetor = {produtos} selecionar = {selecionarProduto}/>
     </div>
   );
--- front/src/Formulario.js
+++ front/src/Formulario.js
@@ -1,4 +1,4 @@
-function Formulario({botao, eventoTeclado, cadastrar, obj}){
+function Formulario({botao, eventoTeclado, cadastrar, obj, cancelar, remover, alterar}){
     return(
         <form>
             <input type='text' value={obj.nome} onChange={eventoTeclado} name = 'nome' placeholder='Nome' className = 'form-control'/>
@@ -11,9 +11,9 @@ function Formulario({botao, eventoTeclado, cadastrar, obj}){
                   <input type='button' value = 'Cadastrar' onClick={cadastrar} className='btn btn-primary'/>
                 :
                 <div>
-                  <input type='button' value = 'Alterar'   className='btn btn-warning'/>
-                  <input type='button' value = 'Remover'   className='btn btn-danger'/>
-                  <input type='button' value = 'Cancelar'  className='btn btn-secondary'/>
+                  <input type='button' value = 'Alterar'  onClick={alterar} className='btn btn-warning'/>
+                  <input type='button' value = 'Remover'  onClick={remover} className='btn btn-danger'/>
+                  <input type='button' value = 'Cancelar' onClick={cancelar} className='btn btn-secondary'/>
                 </div>
              }
 
